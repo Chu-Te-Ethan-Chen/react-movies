188e6dcb568c6756dedd78faf987d7f8dd582b6e
Jordon Chen <chenzhude@chenzhudedeMacBook-Pro.local>
2021-06-20 10:58:49 +0800

    Updates

---
+++ precache-manifest.dd47cf7117b940b1102646f26e7588fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274e9cf6a78f76ef36bf713bef8c0b4c",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "00c680930aa6f23974af",
+    "url": "/react-movies/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "6663c60c89a817973bfd",
+    "url": "/react-movies/static/js/2.f4170690.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-movies/static/js/2.f4170690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c680930aa6f23974af",
+    "url": "/react-movies/static/js/main.cde096e3.chunk.js"
+  },
+  {
+    "revision": "67513ede745015a201e5",
+    "url": "/react-movies/static/js/runtime-main.f2ea0420.js"
+  }
+]);
